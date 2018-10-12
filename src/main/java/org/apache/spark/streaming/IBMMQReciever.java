e3f6b4c4b43303e226cd9ad1e9808a8b79a4ae81
gss2002 <greg@senia.org>
2018-10-11 20:12:51 -0400

    Spark Streaming IBM Native MQ Processor fixes

--- src/main/java/org/apache/spark/streaming/IBMMQReciever.java
+++ src/main/java/org/apache/spark/streaming/IBMMQReciever.java
@@ -9,12 +9,11 @@ import com.ibm.mq.constants.MQConstants;
 
 import org.apache.spark.storage.StorageLevel;
 import org.apache.spark.streaming.receiver.Receiver;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
-import javax.jms.BytesMessage;
-import javax.jms.Message;
 import java.util.Hashtable;
-import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * Created by gsenia
@@ -34,18 +33,23 @@ public class IBMMQReciever extends Receiver<String> {
 	String password = null;
 	MQQueue queue = null;
 	int queueDepth = 0;
-	int openOptions = MQConstants.MQOO_INPUT_AS_Q_DEF | MQConstants.MQOO_OUTPUT | MQConstants.MQOO_INQUIRE;
-	MQGetMessageOptions gmo;
+	int openOptions = MQConstants.MQOO_INPUT_SHARED | MQConstants.MQOO_OUTPUT | MQConstants.MQOO_INQUIRE
+			| MQConstants.MQOO_BROWSE;
 	MQQueueManager qmgr = null;
 	MQMessage rcvMessage;
 	int strLen;
 	byte[] strData;
 	String msgText = null;
-	JSONObject jsonOut;
+	JSONArray jsonOut;
+	JSONObject jsonKey;
+	JSONObject jsonValue;
 	long messagePutMs;
+	int waitInterval = 5000;
 	int seqNo;
+	boolean keepMessages = true;
 
-	public IBMMQReciever(String host, int port, String qmgrName, String channel, String queueName, String userName, String password) {
+	public IBMMQReciever(String host, int port, String qmgrName, String channel, String queueName, String userName,
+			String password, String waitInterval, String keepMessages) {
 		super(StorageLevel.MEMORY_ONLY_2());
 		this.host = host;
 		this.port = port;
@@ -54,16 +58,16 @@ public class IBMMQReciever extends Receiver<String> {
 		this.channel = channel;
 		this.username = userName;
 		this.password = password;
-		this.gmo = new MQGetMessageOptions();
-
-		// The following is needed to provide backward compatibility with MQ V6 Java
-		// code
-		// to properly handle the "format" property of MQHRF2 from JMS messages
-		gmo.options = gmo.options + MQConstants.MQGMO_PROPERTIES_FORCE_MQRFH2;
-		/*
-		 * if (qGMOConvert) { gmo.options = gmo.options + MQConstants.MQGMO_CONVERT; }
-		 */
-
+		if (keepMessages != null) {
+			if (!(keepMessages.equalsIgnoreCase(""))) {
+				this.keepMessages = Boolean.parseBoolean(keepMessages);
+			}
+		}
+		if (waitInterval != null) {
+			if (!(waitInterval.equalsIgnoreCase(""))) {
+				this.waitInterval = Integer.parseInt(waitInterval);
+			}
+		}
 	}
 
 	public void onStart() {
@@ -71,8 +75,8 @@ public class IBMMQReciever extends Receiver<String> {
 		new Thread() {
 			@Override
 			public void run() {
-					initConnection();
-					receive();
+				initConnection();
+				receive();
 			}
 		}.start();
 	}
@@ -94,82 +98,87 @@ public class IBMMQReciever extends Receiver<String> {
 		} catch (MQException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			queue = null;
-			qmgr = null;
 		}
 
-		// There is nothing much to do as the thread calling receive()
-		// is designed to stop by itself isStopped() returns false
 	}
 
 	/** Create a MQ connection and receive data until receiver is stopped */
 	private void receive() {
-		System.out.print("Started receiving messages from MQ");
+		System.out.println("Started receiving messages from MQ");
+		MQGetMessageOptions gmo = new MQGetMessageOptions();
+		gmo.options = gmo.options | MQConstants.MQGMO_CONVERT | MQConstants.MQGMO_PROPERTIES_FORCE_MQRFH2
+				| MQConstants.MQGMO_BROWSE_FIRST;
+		if (!(keepMessages)) {
+			gmo.options = gmo.options | MQConstants.MQGMO_MSG_UNDER_CURSOR;
+		}
+		gmo.matchOptions = MQConstants.MQMO_NONE;
+		gmo.waitInterval = this.waitInterval;
 
 		try {
 			if (queueDepth != 0) {
-				for (int i = 0; i < queueDepth; i++) {
-					rcvMessage = null;
-					msgText = null;
-					rcvMessage = new MQMessage();
-					if (queue.isOpen()) {
+				int lastSeqNo = 0;
+				long lastTs = 0;
+				boolean messagesExist = true;
+				while (messagesExist) {
+					try {
+						rcvMessage = new MQMessage();
 						queue.get(rcvMessage, gmo);
 						strLen = rcvMessage.getMessageLength();
 						strData = new byte[strLen];
 						rcvMessage.readFully(strData);
 						messagePutMs = rcvMessage.putDateTime.getTimeInMillis();
 						seqNo = rcvMessage.messageSequenceNumber;
+						if (lastTs == messagePutMs && seqNo == 1) {
+							seqNo = lastSeqNo + seqNo;
+						}
 						msgText = new String(strData);
-						jsonOut = new JSONObject();
-						jsonOut.put(Long.toString(messagePutMs)+"_"+seqNo, msgText);
+						jsonOut = new JSONArray();
+						jsonKey = new JSONObject();
+						jsonValue = new JSONObject();
+						jsonKey.put("key", Long.toString(messagePutMs) + "_" + seqNo);
+						jsonValue.put("value", msgText);
+						jsonOut.put(jsonKey);
+						jsonOut.put(jsonValue);
 						store(jsonOut.toString());
+						lastTs = messagePutMs;
+						lastSeqNo = seqNo;
+
+						// move cursor to next message
+						gmo.options = MQConstants.MQGMO_CONVERT |MQConstants.MQGMO_PROPERTIES_FORCE_MQRFH2 | MQConstants.MQGMO_WAIT
+								| MQConstants.MQGMO_BROWSE_NEXT;
+						if (!(keepMessages)) {
+							gmo.options = gmo.options | MQConstants.MQGMO_MSG_UNDER_CURSOR;
+						}
+					} catch (MQException e) {
+						if (e.reasonCode == MQConstants.MQRC_NO_MSG_AVAILABLE) {
+							System.out.println("No Messages Available on Queue");
+						}
+						messagesExist = false;
+						stop("No Messages Avaialble on Queue to Read");
+					} catch (IOException ioe) {
+						System.out.println("Error: " + ioe.getMessage());
 					}
-
 				}
+			} else {
+				stop("No Messages Available on Queue to Read");
 			}
-
-			/*
-			 * JMSMessage receivedMessage = null;
-			 * 
-			 * while (!isStopped() && enumeration.hasMoreElements()) {
-			 * 
-			 * receivedMessage = (JMSMessage) enumeration.nextElement(); String userInput =
-			 * convertStreamToString(receivedMessage); //
-			 * System.out.println("Received data :'" + userInput + "'"); store(userInput); }
-			 * 
-			 * // Restart in an attempt to connect again when server is active again //
-			 * restart("Trying to connect again");
-			 */
-			stop("No More Messages To read !");
-			if (queue != null) {
-				if (queue.isOpen()) {
-					queue.close();
-				}
-			}
-			if (qmgr != null) {
-				if (qmgr.isConnected()) {
-					qmgr.close();
-				}
-			}
-
-			System.out.println("Queue Connection is Closed");
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			restart("Trying to connect again");
+			
 		} catch (Throwable t) {
 			// restart if there is any other error
-			restart("Error receiving data", t);
-		} finally {
-			queue = null;
-			qmgr = null;
-			restart("Trying to connect again");
+			restart("Error receiving data from Queue or QMGR", t);
 		}
 
 	}
 
 	public void initConnection() {
-		System.out.println("Initiating Queue Connection!");
+	    final String JAVA_VENDOR_NAME = System.getProperty("java.vendor");
+	    final boolean IBM_JAVA = JAVA_VENDOR_NAME.contains("IBM");
+		System.out.println("Initiating Connection to QMGR and Queue");
+		if (IBM_JAVA) {
+			System.setProperty("com.ibm.mq.cfg.useIBMCipherMappings", "true");
+		} else {
+			System.setProperty("com.ibm.mq.cfg.useIBMCipherMappings", "false");
+		}
 		Hashtable<String, Object> properties = new Hashtable<String, Object>();
 		properties.put(MQConstants.HOST_NAME_PROPERTY, host);
 		properties.put(MQConstants.PORT_PROPERTY, port);
@@ -185,41 +194,22 @@ public class IBMMQReciever extends Receiver<String> {
 			qmgr = new MQQueueManager(qmgrName, properties);
 
 			if (qmgr.isConnected()) {
-				System.out.println("Connected to QMGR: " +qmgrName);
+				System.out.println("Connected to QMGR: " + qmgrName);
 			}
 			if (qmgr != null) {
 				queue = qmgr.accessQueue(queueName, openOptions);
 				if (queue != null) {
 					if (queue.isOpen()) {
-						System.out.println("Connected to Queue: " +queueName);
+						System.out.println("Connected to Queue: " + queueName);
 						queueDepth = queue.getCurrentDepth();
-						System.out.println("Queue Depth: "+queueDepth);
-
+						System.out.println("Queue Depth: " + queueDepth);
 					}
 				}
 			}
 		} catch (MQException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally {
-			queue = null;
-			qmgr = null;
-		}
-
-		/*
-		 * MQQueueConnectionFactory conFactory= new MQQueueConnectionFactory();
-		 * conFactory.setHostName(host); conFactory.setPort(port);
-		 * conFactory.setTransportType(WMQConstants.WMQ_CM_CLIENT);
-		 * conFactory.setQueueManager(qm); conFactory.setChannel(channel);
-		 * 
-		 * qCon= (MQQueueConnection) conFactory.createQueueConnection("username",
-		 * "password"); MQQueueSession qSession=(MQQueueSession)
-		 * qCon.createQueueSession(false, 1); MQQueue queue=(MQQueue)
-		 * qSession.createQueue(qn); qCon.start();
-		 * 
-		 * MQQueueBrowser browser = (MQQueueBrowser) qSession.createBrowser(queue);
-		 * enumeration= browser.getEnumeration();
-		 */
+		} 
 
 		System.out.println("Queue Connection Established successfully!");
 
@@ -228,28 +218,6 @@ public class IBMMQReciever extends Receiver<String> {
 	@Override
 	public StorageLevel storageLevel() {
 		return StorageLevel.MEMORY_ONLY_2();
-	}
-
-	/**
-	 * Convert stream to string
-	 *
-	 * @param jmsMsg
-	 * @return
-	 * @throws Exception
-	 */
-	private static String convertStreamToString(final Message jmsMsg) throws Exception {
-		String stringMessage = "";
-		BytesMessage bMsg = (BytesMessage) jmsMsg;
-		byte[] buffer = new byte[40620];
-		int byteRead;
-		ByteArrayOutputStream bout = new java.io.ByteArrayOutputStream();
-		while ((byteRead = bMsg.readBytes(buffer)) != -1) {
-			bout.write(buffer, 0, byteRead);
-		}
-		bout.flush();
-		stringMessage = new String(bout.toByteArray());
-		bout.close();
-		return stringMessage;
 	}
 
 }
